1838eb089c488b58b9ed79047538ef4719692da0
dmitrygvl <dmitrygv1994@ya.ru>
2024-03-28 18:29:29 +0300

    Add commentController

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const multer = require("multer");
 const UserController = require("../controllers/userController");
 const PostController = require("../controllers/postController");
+const CommentController = require("../controllers/commentController");
 const tokenAuthentication = require("../middleware/auth");
 
 const uploadsDestination = "uploads";
@@ -28,5 +29,13 @@ router.post("./posts", tokenAuthentication, PostController.createPost);
 router.get("./posts", tokenAuthentication, PostController.getAllPosts);
 router.post("./posts/:id", tokenAuthentication, PostController.getPostById);
 router.delete("./posts/:id", tokenAuthentication, PostController.deletePost);
+
+//comments routes
+router.post("/comments", tokenAuthentication, CommentController.createComment);
+router.delete(
+  "/comments/:id",
+  tokenAuthentication,
+  CommentController.deleteComment,
+);
 
 module.exports = router;
